69aebded2b0dd773f74418f141302d2a2facd12b
Yipu Wang <yipu.wang@nike.com>
2023-07-04 14:22:42 +0800

    Embrace Swift Generics

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+protocol AnimalFeed {
+    associatedtype CropType: Crop where CropType.Feed == Self
+    static func grow() -> CropType
+}
+
+protocol Crop {
+    associatedtype Feed: AnimalFeed where Feed.CropType == Self
+    func harvest() -> Feed
+}
+
+protocol Animal {
+    associatedtype Feed: AnimalFeed
+    func eat(_ food: Feed)
+}
+
+struct Farm {
+    func feed(_ animal: some Animal) {
+        let crop = type(of: animal).Feed.grow()
+        let produce = crop.harvest()
+        animal.eat(produce)
+    }
+
+    func feedAll(_ animals: [any Animal]) {
+        for animal in animals {
+            feed(animal)
+        }
+    }
+}
+
+struct Cow: Animal {
+    func eat(_ food: Hay) { }
+}
+
+struct Hay: AnimalFeed {
+    static func grow() -> Alfalfa {
+        Alfalfa()
+    }
+}
+
+struct Alfalfa: Crop {
+    func harvest() -> Hay {
+        Hay()
+    }
+}
